5a5ddea04a8ce21f23669dc245d17a240ac80e62
Eduardo Frei Bruno Gómez <eduardofreib@gmail.com>
2025-06-18 16:12:41 -0500

    Agrego componente Button y corrijo importación para despliegue en Vercel

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,12 @@
 // src/components/ui/Button.jsx
+
+import React from "react";
+
 export default function Button({ children, ...props }) {
   return (
     <button
+      className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-800 transition"
       {...props}
-      className="bg-blue-700 text-white px-4 py-2 rounded-md hover:bg-blue-800 transition"
     >
       {children}
     </button>
