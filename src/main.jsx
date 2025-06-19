23b113bd422e684ebae30f7fb6ae4dbb115b41f5
Eduardo Frei Bruno Gómez <eduardofreib@gmail.com>
2025-06-18 21:48:13 -0500

    Corrige imports y estructura de componentes y main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // O AppContent, según el nombre que uses
-import "./index.css";
+import App from "./App";
+import "./index.css"; // Si tienes Tailwind u otros estilos globales
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
